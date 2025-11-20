42dee116b987ad6d484209b283e81090edcc41dd
b2 <Administrator@PMT2042>
2025-11-20 15:02:29 +0700

    b2

---
+++ New folder (3)/nhapmonlaptrinh/b2/b2.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout << "nhap so a: ";
+	 cin>> a;
+	 cout << "nhap so b: ";
+	 cin >> b;
+	 int c = a * b - a * b - a ;
+	cout << "ket qua la: " << c ;
+}
