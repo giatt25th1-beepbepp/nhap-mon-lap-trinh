281e58dac1e9c03763dd85c1a1d9cbfeadc7221a
b2 <Administrator@PMT2042>
2025-12-03 14:58:54 +0700

    buoi3

---
+++ nhapmonlaptring/b3/b3.cpp
@@ -0,0 +1,41 @@
+// b3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+void cau1() {
+	/*using namespace std;
+	int main()
+	{
+		int r;
+		float pi = 3.14;
+		cout << "nhap ban kinh r: ";
+		cin >> r;
+		cout << "dien tich la: " << 2 * pi * r * r << endl;
+		cout << "chu vi la: " << 2 * pi * r;
+	}*/
+}
+void cau2(){
+//#include <iostream>
+//	using namespace std;
+//	int main()
+//	{
+//		int luongcb;
+//		int luongpc;
+//		cout << "luong co ban: ";
+//		cin >> luongcb;
+//		cout << "luong phu cap: ";
+//		cin >> luongpc;
+//		cout << "tong: " << (luongcb + luongpc) - 0.1;
+//	}
+//}
+#include <iostream>
+using namespace std;
+int main()
+{
+	int luongcb;
+	int luongpc;
+	cout << "luong co ban: ";
+	cin >> luongcb;
+	cout << "luong phu cap: ";
+	cin >> luongpc;
+	cout << "tong: " << (luongcb + luongpc) - 0.1;
+}
+	
