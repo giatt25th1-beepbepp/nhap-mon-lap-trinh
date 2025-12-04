4ad36cbd79ed89a9acf2b1b47c49c652443f1ac8
gia truong <thietgia112006@gmail.com>
2025-12-04 10:48:17 +0700

    buoi4

--- nhapmonlaptring/b4/b4.cpp
+++ nhapmonlaptring/b4/b4.cpp
@@ -1,20 +1,94 @@
 // b4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+ void cau1(){
+	 /*string hoten;
+	 int namsinh;
+	 bool gioitinh;
+	 cout << "nhap ho ten: ";
+	 getline(cin, hoten);
+	 cout << "nhap nam sinh: ";
+	 cin >> namsinh;
+	 cout << "nhap gioi tinh (1-nam, 0-nu): ";
+	 int temp;
+	 cin >> temp;
+	 gioitinh = temp == 1 ? true : false;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	void cau2() {
+		 /*int sotien;
+		 cout << "nhap so tien: ";
+		 cin >> sotien;
+		 int to500k = sotien / 500000;
+		 sotien -= to500k * 500000;
+		 cout << "so to 500k: " << to500k << endl;
+		 int to200k = sotien / 200000;
+		 cout << "so to 200k:" << to200k << endl;
+		 sotien -= to200k * 200000;
+		 int to100k = sotien / 100000;
+		 cout << "so to 100k: " << to100k << endl;
+		 sotien -= to100k * 100000;
+		 int to50k = sotien / 50000;
+		 cout << "so to 50k: " << to50k << endl;
+		 sotien -= to50k * 50000;
+		 int to20k = sotien / 20000;
+		 cout << "so to 20k: " << to20k << endl;
+		 sotien -= to20k * 20000;
+		 int to10k = sotien / 10000;
+   	  cout << "so to 10k: " << to10k << endl;*/
+	 }
+  
+ void cau2() { /*int sotien;
+		 cout << "nhap so tien: ";
+		 cin >> sotien;
+		 int to500k = sotien / 500000;
+		 sotien -= to500k * 500000;
+		 cout << "so to 500k: " << to500k << endl;
+		 int to200k = sotien / 200000;
+		 cout << "so to 200k:" << to200k << endl;
+		 sotien -= to200k * 200000;
+		 int to100k = sotien / 100000;
+		 cout << "so to 100k: " << to100k << endl;
+		 sotien -= to100k * 100000;
+		 int to50k = sotien / 50000;
+		 cout << "so to 50k: " << to50k << endl;
+		 sotien -= to50k * 50000;
+		 int to20k = sotien / 20000;
+		 cout << "so to 20k: " << to20k << endl;
+		 sotien -= to20k * 20000;
+		 int to10k = sotien / 10000;
+	  cout << "so to 10k: " << to10k << endl;*/
+ }
+   void cau3(){
+	   void cau4(); {
+		  /* using namespace std;
+		   int main();
+		   {
+			   int x1, x2, y1, y2;
+			   cout << "nhap toa do diem A (x1, y1): ";
+			   cin >> x1 >> y1;
+			   cout << "nhap toa do diem B (x2, y2): ";
+			   cin >> x2 >> y2;
+			   cout << "khoang cach: " << sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+			   return 0;
+		   }*/
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	   }
+   }
+
+   void cau5() {
+	   /*using namespace std;
+	   int main() {
+		   int a, b, c;
+		   cout << "nhap 3 canh: ";
+		   cin >> a >> b >> c;
+		   cout << "the tich: " << a * b * c << endl;
+		   cout << " dien tich: " << 2 * (a * b + b * c + a * c) << endl;
+	   }*/
+   }
+	  
+  
+	  
+   }
+   
