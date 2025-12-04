c14897e510ed9b2a17b61b3648b18410326df422
Administrator <Administrator@PMT2042>
2025-12-04 12:26:38 +0700

    buoi5.1

---
+++ nhapmonlaptring/b5/b5.cpp
@@ -0,0 +1,36 @@
+// b5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+void cau1(){
+	/*int a, b, c;
+	cout << "nhap a, b, c: ";
+	cin >> a >> b >> c;
+	if (a > b && a > c)
+		cout << a;
+	if (b > a && b > c)
+		cout << b;
+	if (c > a && c > b)
+		cout << c;*/
+}
+void cau2(){
+	//int a, b;
+	//cout << "nhap a, b: ";
+	//cin >> a >> b;
+	//if (a * b > 0)
+	//	cout << "cung dau";
+	//else
+	//	cout << "khac dau";
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "nhap a, b: ";
+	cin >> a >> b;
+	if (a * b > 0)
+		cout << "cung dau";
+	else
+		cout << "khac dau";
+
+}
